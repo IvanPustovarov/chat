7e606489c7c1822247ff4806091458e9bcb4ee76
Winston Smith <silverflash150@gmail.com>
2021-06-30 20:04:11 +0300

    getting static data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+
+import { db } from "./firebase";
+
 import InputSpace from "./components/InputSpace";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    db.collection("messages")
+      .get()
+      .then((snapshot) => {
+        const messages = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setMessages(messages);
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }, []);
   return (
     <div className="App">
+      <ul>
+        {messages.map((message) => (
+          <div key={message.id}>
+            <div style={{ fontWeight: "bold" }}>{message.name}</div>
+            <div>{message.text}</div>
+          </div>
+        ))}
+      </ul>
       <InputSpace />
     </div>
   );
